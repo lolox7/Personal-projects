5d801afa7a78abf16a3a7c2a2653b1358dc9a62f
Loris MARTINEZ <loris.martinez78500@gmail.com>
2025-04-11 13:20:00 +0200

    modifications et ajouts divers

--- Javascript/color_changer.js
+++ Javascript/color_changer.js
@@ -8,10 +8,3 @@ function color_change() {
     document.getElementById("main_color_changer").style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
 return console.log("done");
 }
-
-function letter_counter(){
-    var input = document.getElementById('input').value ;
-    document.getElementById('result').innerHTML = input.length + 1;
-}
-
-window.onload = document.getElementById('result').innerHTML = 0
---
+++ Javascript/letter_counter.js
@@ -0,0 +1,7 @@
+
+function letter_counter(){
+    var input = document.getElementById('input').value ;
+    document.getElementById('result').innerHTML = input.length + 1;
+}
+
+window.onload = document.getElementById('result').innerHTML = 0
